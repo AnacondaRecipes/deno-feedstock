b0685d66445a7c50ab557dc2b5f0a581f4636bde
Daniel Nachun <daniel.nachun@gmail.com>
2026-09-14 18:18:39 -0700

    try to fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ export CARGO_PROFILE_RELEASE_LTO=fat
 export LIBCLANG_PATH=${BUILD_PREFIX}/lib
 
 cargo bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo install --no-default-features --bins --no-track --locked --root ${PREFIX} --path ./cli
+cargo install --bins --no-track --locked --root ${PREFIX} --path ./cli
 
 mkdir -p $PREFIX/etc/conda/activate.d
 echo "export DENO_INSTALL_ROOT=$PREFIX" > "${PREFIX}/etc/conda/activate.d/deno.sh"
